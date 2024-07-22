20b0da35f3f897e5a18c216bf70720924a1b6b54
Mohamed Alaa <mohamed.alaa636@hotmail.com>
2024-07-22 13:06:40 +0300

    Added jks file + modified gitignore

---
+++ app/src/main/java/my/ym/retirementcalculator/utils/SecretConstants.kt
@@ -0,0 +1,11 @@
+package my.ym.retirementcalculator.utils
+
+object SecretConstants {
+
+    object AppCenter {
+
+        const val APP_SECRET = "3b42f701-02c5-4e89-aaab-eb486cfed12c"
+
+    }
+
+}
